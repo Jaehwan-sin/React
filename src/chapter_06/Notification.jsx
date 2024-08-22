530f3b506bb29b10089bfba710aeb018f66eccac
jae hwan shin <fate427@naver.com>
2024-08-23 00:54:45 +0900

    컴포넌트 생명주기, props를 활용한 데이터 전달

--- src/chapter_06/Notification.jsx
+++ src/chapter_06/Notification.jsx
@@ -16,27 +16,41 @@ const styles = {
 };
 
 class Notification extends React.Component {
+    // 컴포넌트의 생성자 함수입니다. props를 받아와서 부모 클래스의 생성자에 전달합니다.
     constructor(props) {
         super(props);
 
+        // 컴포넌트의 상태(state)를 정의합니다. 여기서는 비어 있습니다.
         this.state = {};
     }
 
+    // 컴포넌트가 처음으로 DOM에 마운트된 직후에 호출됩니다.
+    // 이 메서드는 컴포넌트가 화면에 나타난 직후에 실행될 코드를 작성할 수 있는 곳입니다.
     componentDidMount() {
+        // 컴포넌트가 마운트되었을 때 콘솔에 메시지를 출력합니다.
         console.log(`${this.props.id} componentDidMount() called.`);
     }
 
+    // 컴포넌트가 업데이트된 직후에 호출됩니다. 여기서 "업데이트"는 props나 state가 변경된 경우를 의미합니다.
     componentDidUpdate() {
+        // 컴포넌트가 업데이트되었을 때 콘솔에 메시지를 출력합니다.
         console.log(`${this.props.id} componentDidUpdate() called.`);
     }
 
+    // 컴포넌트가 DOM에서 제거되기 직전에 호출됩니다.
+    // 이 메서드는 컴포넌트가 언마운트되기 직전에 실행될 코드를 작성할 수 있는 곳입니다.
     componentWillUnmount() {
+        // 컴포넌트가 언마운트되었을 때 콘솔에 메시지를 출력합니다.
         console.log(`${this.props.id} componentWillUnmount() called.`);
     }
 
+    // 컴포넌트가 화면에 렌더링될 내용을 반환하는 메서드입니다.
     render() {
         return (
+            // wrapper 스타일이 적용된 div 요소를 렌더링합니다.
             <div style={styles.wrapper}>
+                {/* props로 전달된 message 값을 표시하는 span 요소입니다. 
+                    이 텍스트에 messageText 스타일이 적용됩니다. */}
                 <span style={styles.messageText}>{this.props.message}</span>
             </div>
         );
--- src/chapter_06/NotificationList.jsx
+++ src/chapter_06/NotificationList.jsx
@@ -16,50 +16,65 @@ const reservedNotifications = [
     },
 ];
 
+// 타이머 변수를 전역으로 선언합니다. 이는 setInterval로 설정된 타이머를 관리하는 데 사용됩니다.
 var timer;
 
+// NotificationList라는 클래스형 컴포넌트를 정의합니다. 이 컴포넌트는 여러 개의 알림(Notification)을 관리하고 표시합니다.
 class NotificationList extends React.Component {
+    // 생성자 메서드로 컴포넌트의 초기 state를 설정합니다.
     constructor(props) {
         super(props);
 
+        // 초기 state를 빈 notifications 배열로 설정합니다.
         this.state = {
             notifications: [],
         };
     }
 
+    // 컴포넌트가 처음으로 DOM에 마운트된 직후에 호출됩니다.
+    // 여기서 타이머를 설정하고, 예약된 알림을 순차적으로 state에 추가하는 작업을 수행합니다.
     componentDidMount() {
-        const { notifications } = this.state;
+        const { notifications } = this.state; // 현재 state의 notifications 배열을 가져옵니다.
+        // 타이머를 설정하여 1초(1000ms)마다 새로운 알림을 추가합니다.
         timer = setInterval(() => {
             if (notifications.length < reservedNotifications.length) {
-                const index = notifications.length;
+                const index = notifications.length; // 현재 알림 배열의 길이를 인덱스로 사용합니다.
+                // reservedNotifications에서 새로운 알림을 가져와 notifications 배열에 추가합니다.
                 notifications.push(reservedNotifications[index]);
+                // state를 업데이트하여 UI에 변경된 notifications 배열을 반영합니다.
                 this.setState({
                     notifications: notifications,
                 });
             } else {
+                // 모든 알림이 추가된 경우, notifications 배열을 비워 다시 시작할 수 있도록 합니다.
                 this.setState({
                     notifications: [],
                 });
+                // 타이머를 중지합니다.
                 clearInterval(timer);
             }
         }, 1000);
     }
 
+    // 컴포넌트가 DOM에서 제거되기 직전에 호출됩니다.
+    // 여기서는 타이머가 설정되어 있으면 이를 해제하여 메모리 누수를 방지합니다.
     componentWillUnmount() {
         if (timer) {
             clearInterval(timer);
         }
     }
 
+    // 컴포넌트가 화면에 렌더링될 내용을 반환하는 메서드입니다.
     render() {
         return (
+            // notifications 배열에 있는 각 알림을 Notification 컴포넌트로 렌더링합니다.
             <div>
                 {this.state.notifications.map((notification) => {
                     return (
                         <Notification
-                            key={notification.id}
-                            id={notification.id}
-                            message={notification.message}
+                            key={notification.id} // 각 Notification 컴포넌트에 고유한 key를 부여하여 React가 효율적으로 렌더링할 수 있게 합니다.
+                            id={notification.id} // 알림의 id를 Notification 컴포넌트에 전달합니다.
+                            message={notification.message} // 알림의 메시지를 Notification 컴포넌트에 전달합니다.
                         />
                     );
                 })}
@@ -67,5 +82,6 @@ class NotificationList extends React.Component {
         );
     }
 }
+
 
 export default NotificationList;
